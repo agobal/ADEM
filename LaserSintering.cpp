403c21a1d8fb673de6c59451847b8d1fef9fd313
agobal <agobal@gmail.com>
2016-10-20 14:31:52 -0700

    Initialized particle preheat

--- LaserSintering.cpp
+++ LaserSintering.cpp
@@ -10,4 +10,20 @@ using namespace std;
 
 TempProfile LaserSintering(struct PowderBed PB)
 {
-	
+	TempProfile TP;
+	// First step is to initialize the temperature and internal energy matrices
+	float solid_heat_capacity = 40;
+	float particle_heat_capacity;
+	float preheat_temperature = 500;
+	for (int i = 0; i < PB.cell_count; ++i)
+	{
+		for (int j = 0; j < PB.particle_count; ++j)
+		{
+			TP.T[i][j] = preheat_temperature;
+			particle_heat_capacity = solid_heat_capacity*(4.0/3.0)*4.0*atan(1)*pow(PB.r_particles[j], 3);
+			TP.E[i][j] = TP.T[i][j]*particle_heat_capacity;
+		}
+	}
+	return TP;
+}
+
